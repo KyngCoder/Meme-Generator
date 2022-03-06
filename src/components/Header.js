e2d48ea77870453f91037fd4563909c16021c5e4
KyngCoder <keshawnrkmerchant9@gmail.com>
2022-03-06 10:19:39 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import '../App.css'
+
+export default function Header() {
+    return (
+        <header className="header">
+        
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Author: KyngCoder</h4>
+        </header>
+    )
+}
